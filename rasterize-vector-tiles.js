57338ad3a5d075b3304efced4bf11aa7405d9839
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-01-03 13:46:03 +0100

    Use meow for command line parsing

--- rasterize-vector-tiles.js
+++ rasterize-vector-tiles.js
@@ -1,36 +1,74 @@
-const minimist = require("minimist")
+#!/usr/bin/env node
+"use strict"
 const log = require("log-less-fancy")()
+const meow = require("meow")
+const foo = require(".")
+
+const cli = meow(
+  `
+	Usage
+	  $ rasterize-vector-tiles -i <input> -o <output> [options]
+
+	Options
+    --source, -i   Vector tile set in .mbtiles format
+    --target, -o   Raster tile set to be created (.mbtiles)
+    --colorprop    Feature property to use for gray level (0-255), default='value'"
+    --nodata       Colorprop value to interpret as nodata (will not be rendered), default=255"
+    --antialias    Control anti-aliasing (none/default/gray/subpixel), default=none"
+    --zoomlevel    Upper limit on zoom level to rasterize (0-99), default=highest in input file"
+    --bitmapsize   Width/Height of the created rasters, default = 256
+    --png          Export as individual png files rather than .mbtiles archive"
+
+	Examples
+	  $ rasterize-vector-tiles -i vector.mbtiles -o raster.mbtiles -prop value
+	  🌈 unicorns 🌈
+`,
+  {
+    flags: {
+      source: {
+        type: "string",
+        alias: "i"
+      },
+      target: {
+        type: "string",
+        alias: "o"
+      },
+      colorprop: {
+        type: "string",
+        alias: "prop",
+        default: "value"
+      },
+      nodata: {
+        type: "number",
+        default: 255
+      },
+      antialias: {
+        type: "string",
+        default: "none"
+      },
+      zoomlevel: {
+        type: "number",
+        default: 99
+      },
+      bitmapsize: {
+        type: "number",
+        default: 256
+      },
+      png: {
+        type: "boolean",
+        default: false
+      }
+    }
+  }
+)
+
+log.warn(cli)
+
 const render = require("./render")
 const { decodePbf } = require("./mbtiles/pbf/pbf_dump")
 const { readTile, readMetadata, listFiles } = require("./mbtiles/mbtileReader")
 const { writeTile, createMbtile } = require("./mbtiles/mbtileWriter")
 const fs = require("fs")
-
-var argv = minimist(process.argv.slice(2), { alias: { prop: "colorprop" } })
-if (argv._.length !== 2) {
-  console.log("Usage: npx rasterize-vector-tiles <source> <target>")
-  console.log("")
-  console.log("source    Vector tile set in .mbtiles format")
-  console.log("target    Raster tile set to be created (.mbtiles)")
-  console.log("")
-  console.log("Options:")
-  console.log(
-    "   --colorprop value    Feature property to use for gray level (0-255), default='value'"
-  )
-  console.log(
-    "   --nodata    255      Colorprop value to interpret as nodata (will not be rendered), default=255"
-  )
-  console.log(
-    "   --antialias none     Control anti-aliasing (none/default/gray/subpixel), default=none"
-  )
-  console.log(
-    "   --zoomlevel 14       Upper limit on zoom level to rasterize (0-99), default=highest in input file"
-  )
-  console.log(
-    "   --png                Export as individual png files rather than .mbtiles archive"
-  )
-  process.exit(1)
-}
 
 const metadata = {
   name: "",
@@ -40,25 +78,12 @@ const metadata = {
   format: "png"
 }
 
-const srcvtiles = argv._[0]
-const targetMbtilePath = argv._[1]
+const option = cli.flags
+//console.log(option)
 
-async function createDatabase(targetMbtilePath) {
-  return await createMbtile(targetMbtilePath, metadata)
-}
-
-const option = {
-  bitmapsize: 256,
-  colorprop: argv.colorprop || "value",
-  nodata: argv.nodata || 255,
-  antialias: argv.antialias || "none",
-  zoomlevel: argv.zoomlevel || 99,
-  png: argv.png
-}
-
-console.log(option)
-async function rasterize(srcvtiles, targetdb) {
-  const files = await listFiles(srcvtiles, option.zoomlevel)
+async function rasterize(targetdb) {
+  log.info("Reading from " + option.source)
+  const files = await listFiles(option.source, option.zoomlevel)
   console.log("Source tile count: " + files.length)
   for (let i = 0; i < files.length; i++) await rasterizeTile(files[i], targetdb)
   targetdb.close()
@@ -76,11 +101,11 @@ async function rasterizeTile(vtile, targetdb) {
   else await writeTile(targetdb, zoom_level, tile_column, tile_row, imageBuffer)
 }
 
-async function convert(targetMbtilePath) {
-  const targetdb = await createDatabase(targetMbtilePath)
-  await rasterize(srcvtiles, targetdb)
+async function convert() {
+  const targetdb = await createMbtile(option.target, metadata)
+  await rasterize(targetdb)
 }
 
-convert(targetMbtilePath).then((reject, resolve) => {
+convert().then((reject, resolve) => {
   if (reject) throw new Error(reject)
 })
