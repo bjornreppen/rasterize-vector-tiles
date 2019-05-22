c65211b67204cca56e7021b4715438258e2d2eb3
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-22 22:18:46 +0200

    Avoid using node 12 API stdout.clearLine
    Fixes #3

--- printprogress.js
+++ printprogress.js
@@ -1,25 +1,24 @@
-let lastUpdate = 0
+let lastUpdate = 0;
 
 // Updates the single line progress indicator on screen
 
 function printprogress(start, tilesComplete, totalTiles) {
-  const now = new Date()
-  if (now - lastUpdate < 1000) return // Update frequency < once per second
-  const elapsedMs = new Date() - start
-  const progress = (100 * tilesComplete) / totalTiles
-  const remainMs = (elapsedMs / tilesComplete) * (totalTiles - tilesComplete)
-  const tilespersec = (tilesComplete / elapsedMs) * 1000
-  const est = new Date(Date.now() + remainMs)
-  process.stdout.clearLine()
-  process.stdout.cursorTo(0)
+  const now = new Date();
+  if (now - lastUpdate < 1000) return; // Update frequency < once per second
+  const elapsedMs = new Date() - start;
+  const progress = (100 * tilesComplete) / totalTiles;
+  const remainMs = (elapsedMs / tilesComplete) * (totalTiles - tilesComplete);
+  const tilespersec = (tilesComplete / elapsedMs) * 1000;
+  const est = new Date(Date.now() + remainMs);
+  process.stdout.cursorTo(0);
   process.stdout.write(
     `${tilesComplete}/${totalTiles} (${progress.toFixed(
       2
     )}%)\t${tilespersec.toFixed(
       2
     )} tiles/sec\t Completion est: ${est.toLocaleTimeString()}`
-  )
-  lastUpdate = now
+  );
+  lastUpdate = now;
 }
 
-module.exports = printprogress
+module.exports = printprogress;
