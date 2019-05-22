be413a57e8e8460aad0e1b047ea71a5f7a7873d2
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-22 21:10:56 +0200

    Avoid relying on node v10+ APIs
    Fixes #3

--- target/pngfiles.js
+++ target/pngfiles.js
@@ -1,20 +1,20 @@
 // Writes rendered tiles to separate png fiels in zyx directory structure
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs-extra");
+const path = require("path");
 
 class PngFiles {
   async create(filePath) {
-    this.path = filePath
+    this.path = filePath;
   }
 
   async createTile(zoom_level, tile_column, tile_row, imageBuffer) {
-    const directory = path.join(this.path, `${zoom_level}/${tile_column}`)
-    const fullPath = path.join(directory, `${tile_row}.png`)
-    fs.mkdirSync(directory, { recursive: true })
-    fs.writeFileSync(fullPath, imageBuffer)
+    const directory = path.join(this.path, `${zoom_level}/${tile_column}`);
+    const fullPath = path.join(directory, `${tile_row}.png`);
+    fs.ensureDirSync(directory);
+    fs.writeFileSync(fullPath, imageBuffer);
   }
 
   close() {}
 }
 
-module.exports = PngFiles
+module.exports = PngFiles;
